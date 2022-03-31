895378acbca6900b74f331eaeaeb84e50811522f
Son Jongchan <jcson.pro@gmail.com>
2022-03-31 11:48:36 +0900

    회원가입, 로그인, 로그아웃 완성

--- pybo/views/auth_views.py
+++ pybo/views/auth_views.py
@@ -1,12 +1,21 @@
-from flask import Blueprint, render_template, request, url_for, flash
-from werkzeug.security import generate_password_hash
+from flask import Blueprint, render_template, request, url_for, flash, session, g
+from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import redirect
 
 from .. import db
-from ..forms import UserCreateForm
+from ..forms import UserCreateForm, UserLoginForm
 from ..models import User
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = User.query.get(user_id)
 
 
 @bp.route('/signup/', methods=('GET', 'POST'))
@@ -24,3 +33,27 @@ def signup():
         else:
             flash('이미 존재하는 사용자 입니다')
     return render_template('auth/signup.html', form=form)
+
+
+@bp.route('/signin/', methods=('GET', 'POST'))
+def signin():
+    form = UserLoginForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        error = None
+        user = User.query.filter_by(username=form.username.data).first()
+        if not user:
+            error = "존재하지 않는 사용자 입니다. "
+        elif not check_password_hash(user.password, form.password.data):
+            error = "비밀번호가 올바르지 않습니다"
+        if error is None:
+            session.clear()
+            session['user_id'] = user.id
+            return redirect(url_for('main.index'))
+        flash(error)
+    return render_template('auth/signin.html', form=form)
+
+
+@bp.route('/signout/')
+def signout():
+    session.clear()
+    return redirect(url_for('main.index'))
